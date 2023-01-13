6a1b08d8babb64e96e6a627c456c19d08290c586
Sabrina-Incinga <sabrinagincinga@gmail.com>
2023-01-13 18:04:15 -0300

    excepciones

---
+++ Clase5/Excepciones/LibEscuela/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibEscuela
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Apellido { get; set; }
+
+        private string Nombre;
+        public string nombre
+        {
+            get { return Nombre; }
+            set {
+                if (value.Trim().Length < 1)
+                {
+                    throw new Exception("El nombre no puede estar vacío");
+                }
+                else if (value.Trim().Length > 50)
+                {
+                    throw new Exception("El nombre no puede tener más de 50 caracteres");
+                }
+                else
+                {
+                    Nombre = value.Trim();
+                }
+            }
+        }
+
+    }
+}

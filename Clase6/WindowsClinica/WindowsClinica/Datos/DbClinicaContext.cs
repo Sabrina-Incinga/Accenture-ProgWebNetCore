1fe7722cc22a934507416df84bdb2c1fa2f3a816
Sabrina-Incinga <sabrinagincinga@gmail.com>
2023-01-16 17:34:48 -0300

    ejercicio 2

---
+++ Clase6/WindowsClinica/WindowsClinica/Datos/DbClinicaContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsClinica.Models;
+
+namespace WindowsClinica.Datos
+{
+    internal class DbClinicaContext : DbContext
+    {
+        public DbClinicaContext() : base("KeyDb")
+        {
+        }
+
+        public DbSet<Clinica> Clinicas { get; set; }
+
+        public DbSet<Habitacion> Habitaciones { get; set; }
+    }
+}
